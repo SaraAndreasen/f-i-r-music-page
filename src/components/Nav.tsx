b3bf1b669c55c23e5c93cdf7a1c65defe5907d2c
SaraAndreasen <55578265+SaraAndreasen@users.noreply.github.com>
2024-06-06 01:07:02 +0200

    Add typescript, routing, cleanup

---
+++ src/components/Nav.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "../assets/img/logo.png";
+
+export function MainNavigation() {
+  return (
+    <nav id="main-navigation">
+      <img src={logo} alt="Falling in Reverse logo" />
+    </nav>
+  );
+}
---
+++ src/routes/Root.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { MainNavigation } from "../components/Nav.tsx";
+
+export function Root() {
+  return (
+    <>
+      <MainNavigation />
+      <h1>Testin Root stuffies</h1>
+    </>
+  );
+}

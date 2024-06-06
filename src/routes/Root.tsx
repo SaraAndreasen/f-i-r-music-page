5ff1e35c24b848153489a531e242500e65fe9649
SaraAndreasen <55578265+SaraAndreasen@users.noreply.github.com>
2024-06-07 00:05:04 +0200

    shrink h1 on scroll

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,28 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Header } from "../components/Header.tsx";
 import ronnieRedStripe from "../assets/img/ronnie-1.jpg";
+import { Albums } from "../components/Albums.tsx";
 export function Root() {
+  const [fontSize, setFontSize] = useState<number>(8.5); // Initial font size in em
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const newFontSize = Math.max(4, 8.5 - scrollY / 100); // Adjust the divisor to control the rate of shrinking
+
+      setFontSize(newFontSize);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleResize = () => {
+      console.log("Window width:", window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <>
       <Header />
       <div className="root-body">
-        <h1 className="text-7xl  md:text-8xl lg:text-9xl">
-          Falling in Reverse
-        </h1>
+        <header>
+          <h1
+            className="header-title"
+            style={{ fontSize: `${fontSize}vw`, color: "#eb3022" }} // Apply the calculated font size and other dynamic styles
+          >
+            Falling in Reverse
+          </h1>
+        </header>
+
         <img
           src={ronnieRedStripe}
           alt="Ronnie with red stripe"
-          className="root-ronnie1 max-w-100"
+          className="root-ronnie1"
         />
-        <h2>Albums</h2>
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
+      </div>
+      <div>
+        <Albums />
       </div>
     </>
   );

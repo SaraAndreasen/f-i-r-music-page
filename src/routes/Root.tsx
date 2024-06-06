81740bd146f8352c226d64f1ce23bacbffbe8ce8
SaraAndreasen <55578265+SaraAndreasen@users.noreply.github.com>
2024-06-06 20:59:11 +0200

    CSS changes, add tailwind.css, replace image

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { Header } from "../components/Header.tsx";
-import logo from "../assets/img/logo.png";
 import ronnieRedStripe from "../assets/img/ronnie-1.jpg";
-
 export function Root() {
   return (
     <>
       <Header />
       <div className="root-body">
-        <h1>Falling in Reverse</h1>
+        <h1 className="text-7xl  md:text-8xl lg:text-9xl">
+          Falling in Reverse
+        </h1>
         <img
           src={ronnieRedStripe}
           alt="Ronnie with red stripe"
-          className="root-ronnie1"
+          className="root-ronnie1 max-w-100"
         />
         <h2>Albums</h2>
         <ul>

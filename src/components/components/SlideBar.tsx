1076a2ad0e9587818f4daad85628d0261d77abcc
SaraAndreasen <55578265+SaraAndreasen@users.noreply.github.com>
2024-06-09 01:59:01 +0200

    cleanup, create more components

---
+++ src/components/components/SlideBar.tsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { albums, AlbumsInterface } from "../shared/albums";
+
+export function SlideBar() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [showContent, setShowContent] = useState(false);
+
+  const openNav = () => {
+    setIsOpen(true);
+    setTimeout(() => {
+      setShowContent(true);
+    }, 250); // Delay for content to appear after sidebar animation
+  };
+
+  const exitNav = () => {
+    setIsOpen(false);
+    setTimeout(() => {
+      setIsOpen(false);
+    }, 250); // Delay for sidebar to fully close before hiding it
+  };
+
+  useEffect(() => {
+    if (!isOpen) {
+      setShowContent(false);
+    }
+  }, [isOpen]);
+
+  return (
+    <>
+      {/* Open Navigation Button */}
+      <div className="flex cursor-pointer" onClick={openNav}>
+        <FontAwesomeIcon icon={faBars} className="text-gray-400" />
+      </div>
+
+      {/* Sidebar Overlay */}
+      <div
+        id="sideBar"
+        className={`fixed top-0 right-0 w-full h-full z-10 transition-all duration-500 ${
+          isOpen ? "bg-black bg-opacity-50" : "bg-transparent w-0"
+        }`}
+        style={{ visibility: isOpen ? "visible" : "hidden" }}
+      >
+        {/* Side Navigation */}
+        <div
+          id="sideNav"
+          className={`fixed top-0 right-0 h-full z-20 bg-gradient-to-r from-red-600 to-red-800 transition-all duration-500 ${
+            isOpen ? "w-3/5" : "w-0"
+          }`}
+        >
+          {/* Close Button */}
+          <div
+            onClick={exitNav}
+            className={`text-xl absolute top-0 right-0 mr-8 mt-7 cursor-pointer transition-opacity duration-100 ${
+              showContent ? "opacity-100" : "opacity-0"
+            }`}
+          >
+            <FontAwesomeIcon icon={faXmark} className="text-black" />
+          </div>
+
+          <ul
+            className={`text-2xl sm:text-3xl flex flex-col items-center justify-center h-full transition-opacity duration-100 ${
+              showContent ? "opacity-100" : "opacity-0"
+            }`}
+          >
+            {albums.map((album: AlbumsInterface) => (
+              <li className="p-2 text-black">
+                <a href={album.link}>{album.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,46 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Header } from "../components/components/Header";
-import ronnieRedStripe from "../assets/img/ronnie-1.jpg";
 import { Albums } from "../components/components/AlbumContainer";
+import { HeroBanner } from "../components/components/HeroBanner";
 
 export function Root() {
-  const [fontSize, setFontSize] = useState<number>(7.5); // Initial font size in em
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollY = window.scrollY;
-      const newFontSize = Math.max(4, 7.5 - scrollY / 100); // Adjust the divisor to control the rate of shrinking
-
-      setFontSize(newFontSize);
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
     <>
       <Header />
-      <div className="root-body">
-        <header>
-          <h1
-            className="header-title"
-            style={{ fontSize: `${fontSize}vw`, color: "#eb3022" }} // Apply the calculated font size and other dynamic styles
-          >
-            Falling in Reverse
-          </h1>
-        </header>
-        <img
-          src={ronnieRedStripe}
-          alt="Ronnie with red stripe"
-          className="root-ronnie1"
-        />
-      </div>
+      <HeroBanner />
       <div className="content-break"></div>
-      <div>
-        <Albums />
-      </div>
+      <Albums />
     </>
   );
 }

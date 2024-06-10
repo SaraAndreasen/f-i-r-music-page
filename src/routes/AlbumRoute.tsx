019f70a5467286ea75043f3505e62436988a3fdc
SaraAndreasen <55578265+SaraAndreasen@users.noreply.github.com>
2024-06-10 02:50:13 +0200

    add footer

--- src/routes/AlbumRoute.tsx
+++ src/routes/AlbumRoute.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Header } from "../components/components/Header";
 import { Albums } from "../components/components/AlbumContainer";
+import { Footer } from "../components/components/Footer";
 
 export function SelectedAlbum() {
   return (
     <>
       <Header />
       <Albums />
+      <Footer />
     </>
   );
 }
